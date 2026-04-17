43ef1ec24207b7a73b0a1dcfbf3e734179bee6ff
ZeiAb <zrilwaan10@gmail.com>
2026-04-17 16:50:21 +0100

    update to poolclientId

--- items-app/src/cognito.ts
+++ items-app/src/cognito.ts
@@ -2,7 +2,7 @@ export const cognitoConfig = {
   Auth: {
     Cognito: {
       userPoolId: "us-east-1_k0VRguzbj",
-      userPoolClientId: "31s197goop2h99amdrmbp5dbln",
+      userPoolClientId: "3ls197goop2h99amdrmbp5dbln",
       loginWith: {
         email: true,
       },
